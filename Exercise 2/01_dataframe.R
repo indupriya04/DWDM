baa716b348e416a3036066ee90bb35a2e9f7227d
indupriya04 <112241155+indupriya04@users.noreply.github.com>
2023-01-11 21:45:49 -0800

    Delete Exercise 2 directory

--- Exercise 2/01_dataframe.R
+++
@@ -1,6 +0,0 @@
-names<-c("Ram","Shyam","Kumar")
-age<-c(23,24,35)
-marks<-c(88,78,25)
-df<-data.frame(names,age,marks)
-View(df)
-write.csv(df,"datafr.csv")
--- Exercise 2/04_mode.R
+++
@@ -1,5 +0,0 @@
-names<-c("Ram","shyam","Kumar")
-age<-c(23,24,35)
-marks<-c(88,78,25)
-df<-data.frame(names,age,marks)
-mode(df $age)
